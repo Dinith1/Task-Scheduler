2dcec83ffbaec286a076d788861000f99a467ca9
Hong <hongye1007@gmail.com>
2019-08-20 13:50:23 +1200

    added CostFunc comparator

--- src/main/java/se306/algorithm/CostFunctionComparator.java
+++ src/main/java/se306/algorithm/CostFunctionComparator.java
@@ -1,4 +1,18 @@
 package se306.algorithm;
 
-public class CostFunctionComparator {
+import java.util.Comparator;
+
+public class CostFunctionComparator implements Comparator<PartialSchedule> {
+
+    public int compare(PartialSchedule s1, PartialSchedule s2){
+        if(s1.getCostFunction()<s2.getCostFunction()){
+            return -1;
+        }
+        else if(s1.getCostFunction() == s2.getCostFunction()){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
 }
