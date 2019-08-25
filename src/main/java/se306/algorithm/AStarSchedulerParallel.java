a5b70b756c2a622af402271c302d9d186a876fb3
Hong <hongye1007@gmail.com>
2019-08-25 13:12:22 +1200

    parallel

--- src/main/java/se306/algorithm/AStarSchedulerParallel.java
+++ src/main/java/se306/algorithm/AStarSchedulerParallel.java
@@ -1,20 +1,36 @@
-package se306.algorithm;
-
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveAction;
-import java.util.concurrent.RecursiveTask;
-
-public class AStarSchedulerParallel  {
-    public static ForkJoinPool fjPool = ForkJoinPool.commonPool();
-    private int numCores;
-
-
-public class ForkPartialSchedule extends RecursiveAction{
-
-    @Override
-    protected void compute(){
-
-    }
-}
-
-}
+//package se306.algorithm;
+//
+//import se306.input.CommandLineParser;
+//import se306.output.OutputFileGenerator;
+//import se306.visualisation.backend.ScheduleParser;
+//
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.List;
+//import java.util.PriorityQueue;
+//import java.util.concurrent.ForkJoinPool;
+//import java.util.concurrent.ForkJoinTask;
+//import java.util.concurrent.RecursiveAction;
+//import java.util.concurrent.RecursiveTask;
+//
+//import static sun.tools.jconsole.Messages.THRESHOLD;
+//
+//public class AStarSchedulerParallel extends RecursiveAction<PartialSchedule> {
+//
+//    public static ForkJoinPool fjPool = ForkJoinPool.commonPool();
+//    public static int numOfProcessors = 2;
+//    private static int NUMCORES = 4;
+//    private volatile int usedCores = 1;
+//    private volatile PriorityQueue<PartialSchedule> open = new PriorityQueue<>(new CostFunctionComparator());
+//    private HashSet<PartialSchedule> createdSchedules = new HashSet<>();
+//    private PriorityQueue<PartialSchedule> optimal = new PriorityQueue<>(new CostFunctionComparator());
+//
+//    @Override
+//    protected PartialSchedule compute() {
+//
+//
+//
+//
+//        return optimal.peek();
+//    }
+//}
